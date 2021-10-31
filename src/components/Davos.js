b031f60e835d69bfdd27ed05b0179b7e2759c240
Vladimir <Shamoointhedark@gmail.com>
2021-11-01 02:52:01 +0300

    davosCard/davosMain:concept + partners

--- src/components/Davos.js
+++ src/components/Davos.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Header from './Header';
 import DavosSection from './DavosSection';
+import DavosMain from './DavosMain';
+
 
 class Davos extends React.Component{
   render() {
     return(
       <div className='wrap_davos'>
           <Header />
-          <DavosSection/>
+          <DavosSection />
+          <DavosMain />
       </div>
     )
   }
